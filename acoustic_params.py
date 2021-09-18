4b7246ce9c35f5e731ec1495a1bc25dc34050e05
martinBmeza <camartinbu@gmail.com>
2021-09-18 15:51:11 -0300

    aumentacion terminada

---
+++ acoustic_params.py
@@ -0,0 +1,84 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import librosa
+from lundeby import lundeby
+from scipy import stats
+
+class Acoustic_params():
+    ''' '''
+    def __init__(self, impulse, fs):
+        self.impulse = impulse
+        self.fs = fs
+        self.temporal_decompose()
+
+    def temporal_decompose(self, tau=0.0025):
+        rir = self.impulse / max(abs(self.impulse))
+
+        t_d = np.argmax(abs(rir)) # direct path
+        t_o = int(tau * self.fs) # tolerance window
+        
+        self.init_idx = t_d - t_o
+        self.final_idx = t_d + t_o + 1
+        
+        if self.init_idx < 0:
+            self.init_idx = 0
+        if self.final_idx > len(rir)-1:
+            self.final_idx = len(rir)-1
+
+        # split response in delay - early - late 
+        self.delay = rir[:self.init_idx]
+        self.early = rir[self.init_idx:self.final_idx]
+        self.late = rir[self.final_idx:]
+        
+        self.time = np.linspace(0, len(rir)/self.fs, len(rir))
+        return
+
+    def plotear_banda(self, axs, signal, **kwargs):
+        axs.plot(signal, **kwargs)
+        axs.legend()
+
+
+    def reverberation_time(self, filterbank, plot=False):
+        # Uso solo la parte late para sacar el TR
+        bands = filterbank.apply(self.late)
+       
+        init = 0
+        end = -30
+        factor = 2.0
+        tr = []
+
+        if plot: 
+            fig, axs = plt.subplots(len(bands), figsize=(15,25))
+
+        for i,band in enumerate(bands):
+            noise_onset = lundeby(band, self.fs)
+            filtered_signal = band[:noise_onset+1]
+            abs_signal = np.abs(filtered_signal) / np.max(np.abs(filtered_signal))
+            abs_completa = np.abs(band) / np.max(np.abs(band))
+            
+            # Schroeder integration
+            sch = np.cumsum(abs_signal[::-1]**2)[::-1]
+            sch_db = 10.0 * np.log10(sch / np.max(sch))
+            
+            if plot:
+                #ploteos
+                self.plotear_banda(axs[i], sch_db, label='Schroeder')
+                self.plotear_banda(axs[i], librosa.amplitude_to_db(abs_completa), label='Completa')
+                self.plotear_banda(axs[i], librosa.amplitude_to_db(abs_signal), label='Filtrada')
+                        
+
+            # Linear regression
+            sch_init = sch_db[np.abs(sch_db - init).argmin()]
+            sch_end = sch_db[np.abs(sch_db - end).argmin()]
+            init_sample = np.where(sch_db == sch_init)[0][0]
+            end_sample = np.where(sch_db == sch_end)[0][0]
+            x = np.arange(init_sample, end_sample + 1) / self.fs
+            y = sch_db[init_sample:end_sample + 1]
+            slope, intercept = stats.linregress(x, y)[0:2]
+
+            # Reverberation time (T30, T20, T10 or EDT)
+            db_regress_init = (init - intercept) / slope
+            db_regress_end = (end - intercept) / slope
+            tr.append(factor * (db_regress_end - db_regress_init))
+            #import pdb; pdb.set_trace()
+        return tr  
---
+++ load.py
@@ -0,0 +1,20 @@
+import librosa 
+import matplotlib.pyplot as plt
+import numpy as np
+import soundfile as sf
+
+
+class RIR():
+    """ Main class for the room impulse responses """
+
+    def __init__(self,path, fs=None):
+        self.path = path
+        self.fs = fs
+        self._load_impulso()
+
+    def _load_impulso(self, tau=0.0025):
+        rir, self.fs = librosa.load(self.path, sr=self.fs, dtype='float64')
+        self.rir = rir / max(abs(rir))
+        
+if __name__ == '__main__':
+    impulso = RIR('files/rir.wav')
